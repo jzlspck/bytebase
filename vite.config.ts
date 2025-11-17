8bb65ee32e47b70199add8e711c35d2603855241
fan-two <j2406234743@outlook.com>
2025-11-17 09:28:22 +0800

    提交

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     proxy: {
-      '/login/oauth/access_token': {
-        target: 'https://github.com',
+      "/login/oauth/access_token": {
+        target: "https://github.com",
         changeOrigin: true,
-        secure: true,
+        secure: false,
       },
-    }
+    },
   },
-  base: '/bytebase/',
-})
+  base: command === "serve" ? "/" : "/bytebase/",
+}));
